23bd87922adc61f2460638c48a4ef03c3d339047
Satyam Singh <69614044+pksatyam777@users.noreply.github.com>
2023-10-16 10:32:45 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,22 @@
+// Create a new div element
+var widgetDiv = document.createElement("div"); 
+
+// Create a new button element
+var button = document.createElement("button");
+button.innerHTML = "Display Date and Time";
+
+// Append the button to the div
+widgetDiv.appendChild(button);
+
+// Add an event listener to the button
+button.addEventListener("click", function() {
+  var date = new Date();
+  var timeString = date.toLocaleTimeString();
+  var dateString = date.toLocaleDateString();
+  
+  // Display the date and time
+  alert("Current Date: " + dateString + "\nCurrent Time: " + timeString);
+});
+
+// Append the div to the body of the document
+document.body.appendChild(widgetDiv);
